5efda8388bdcfbc3d299dd693daaeb6d956df84d
Falcao1992 <eduardo.marselle@gmail.com>
2021-06-19 03:21:13 +0200

    add env port to server

--- index.js
+++ index.js
@@ -3,26 +3,24 @@ const cors = require('cors');
 const app = express()
 
 app.use(cors())
-
 app.use(express.json());
 app.use(
     express.urlencoded({
         extended: true,
     }),
 );
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-    console.log('route depart')
-});
 
 app.use('/user', require('./router/user'))
 app.use('/conversation', require('./router/conversation'))
 app.use('/message', require('./router/message'))
 
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+    console.log('route depart')
+});
 
+const port = 8000
 
-const port = 8080
-
-app.listen(port, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log(`Listening on port ${port}`)
 })
